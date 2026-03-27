1b94e2bc0993f363b1c49e581d6a71ff4dbb39a4
Genaldo44 <59778376+Genaldo44@users.noreply.github.com>
2026-03-27 15:28:28 -0300

    Exemplo 01 - Aula de C#

---
+++ exemp_01/exemp_01/Program.cs
@@ -0,0 +1,12 @@
+﻿//Console.WriteLine("Hello, World!");
+
+namespace exemp_01
+{
+        class Program
+    {
+        static void Main(string[] Args) { 
+                        Console.WriteLine("Ola,\n Turma \n de \n Logica");
+        }
+
+    }
+}
